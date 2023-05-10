cd0229758264e52e0f927aa88577d1570157ccaf
Mikhail Moskovkin <anstane228@gmail.com>
2023-05-10 14:10:14 +0300

    Some upgrades

--- bot_assistant.py
+++ bot_assistant.py
@@ -1,40 +1,45 @@
-# Updates on development branch
-
 import os
-import requests
 
 from aiohttp import ClientSession
 
-from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
-    CommandHandler, 
-    MessageHandler, 
+    CommandHandler,
+    MessageHandler,
     filters
 )
+
 from dotenv import load_dotenv
+
+import translators as ts
 
 
 load_dotenv()
-tg_token = os.getenv('TELEGRAM_TOKEN')
+telegram_token = os.getenv('TELEGRAM_TOKEN')
 weather_token = os.getenv('WEATHER_TOKEN')
 
 
-bot = ApplicationBuilder().token(tg_token).build()
-
+bot = ApplicationBuilder().token(telegram_token).build()
 client_session = {}
 
 
-async def start_com(update, context):
-    text = 'Hello, great to see you! I`m your local GPT-bot.'
+async def start(update, context):
+    """Ответ на /start."""
+    text = 'Hello, great to see you! Have a nice day!'
     await update.message.reply_text(text)
 
 
+# Нужно сделать, чтобы это было единственной кнопкой.
+# Возможно сделать кнопками самые частые запросы.
 async def get_city_name(update, context):
-    await update.message.reply_text('Please enter the name of a city to get the current weather forecast:')
+    """Ответ на /weather_forecast."""
+    text = 'Please enter the name of a city to get the current weather forecast:'
+    await update.message.reply_text(text)
 
 
+# Нужно будет обработать вариант, когда города не существует.
 async def get_weather_forecast(city):
+    """Получаем JSON с погодой и возвращаем его."""
     endpoint = 'https://api.openweathermap.org/data/2.5/weather'
     params = {'q': city, 'appid': weather_token}
 
@@ -43,19 +48,42 @@ async def get_weather_forecast(city):
     async with client_session['session'].get(url=endpoint, params=params) as response:
         weather_json = await response.json()
 
-    return weather_json
+    try:
+        return weather_json['weather'][0]['main']
+    except KeyError: # Переделать на корректные try/except + добавить логгировние.
+        return 'Data is empty.'
 
 
-async def weather_forecast(update, context):
-    city = update.message.text
-    weather_forecast = await get_weather_forecast(city)
-    await update.message.reply_text(weather_forecast)
+# async def formating_weather_forecast(weather):
+#     """Преобразуем итоговый результат."""
+#     weather_forecast = []
+#     temperature = round(weather['main']['temp'] - 273.15, 1)
+#     weather_forecast.append(temperature)
 
-bot.add_handler(CommandHandler('start', start_com))
+#     return weather_forecast
+
+
+async def translate_city_name(city):
+    return ts.translate_text(city, translator='google', from_language='ru')
+
+
+async def translate_weather_forecast(weather):
+    """Переводчик."""
+    return ts.translate_text(weather, translator='google', to_language='ru')
+
+
+async def main(update, context):
+    """Реализация основной функции."""
+    traslated_city = await translate_city_name(update.message.text)
+    weather_json = await get_weather_forecast(traslated_city)
+    weather = await translate_weather_forecast(weather_json)
+    await update.message.reply_text(weather)
+
+
+bot.add_handler(CommandHandler('start', start))
 bot.add_handler(CommandHandler('weather_forecast', get_city_name))
-bot.add_handler(MessageHandler(filters.ALL, weather_forecast))
+bot.add_handler(MessageHandler(filters.TEXT, main))
 
 
 if __name__ == '__main__':
-    print('Bot started.')
     bot.run_polling()
--- draft.py
+++ draft.py
@@ -1,29 +1,23 @@
 import os
-import requests
 
-from aiohttp import ClientSession
+from dotenv import load_dotenv
 
-from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler, 
     MessageHandler, 
     filters
 )
-from dotenv import load_dotenv
 import openai
 
 
 load_dotenv()
 tg_token = os.getenv('TELEGRAM_TOKEN')
 gpt_token = os.getenv('OPENAI_TOKEN')
-weather_token = os.getenv('WEATHER_TOKEN')
 
 
 openai.api_key = gpt_token
 bot = ApplicationBuilder().token(tg_token).build()
-
-client_session = {}
 
 
 async def start_com(update, context):
@@ -41,30 +35,7 @@ async def gpt_bot(update, context):
     await update.message.reply_text(response.choices[0].text)
 
 
-async def get_city_name(update, context):
-    await update.message.reply_text('Please enter the name of a city to get the current weather forecast:')
-
-
-async def get_weather_forecast(city):
-    endpoint = 'https://api.openweathermap.org/data/2.5/weather'
-    params = {'q': city, 'appid': weather_token}
-
-    client_session['session'] = ClientSession()
-
-    async with client_session['session'].get(url=endpoint, params=params) as response:
-        weather_json = await response.json()
-
-    return weather_json
-
-
-async def weather_forecast(update, context):
-    city = update.message.text
-    weather_forecast = await get_weather_forecast(city)
-    await update.message.reply_text(weather_forecast)
-
 bot.add_handler(CommandHandler('start', start_com))
-bot.add_handler(CommandHandler('weather_forecast', get_city_name))
-bot.add_handler(MessageHandler(filters.ALL, weather_forecast))
 bot.add_handler(MessageHandler(filters.ALL, gpt_bot))
 
 
